3b5801caec268e434d39124cecf7dbfb1b6cf96e
Rhainds <202255091@collegeahuntsic.qc.ca>
2024-08-30 12:32:11 -0400

    Initial Commit

---
+++ RhaindsEmile_testProject/RhaindsEmile_testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhaindsEmile_testProjectApp.swift
+//  RhaindsEmile_testProject
+//
+//  Created by Émile Rhainds (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct RhaindsEmile_testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RhaindsEmile_testProjectUITests/RhaindsEmile_testProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RhaindsEmile_testProjectUITestsLaunchTests.swift
+//  RhaindsEmile_testProjectUITests
+//
+//  Created by Émile Rhainds (Étudiant) on 2024-08-30.
+//
+
+import XCTest
+
+final class RhaindsEmile_testProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
